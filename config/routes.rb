5aad323d1f261ec7ad5a4193a7c6694fe75eb0c6
Jordan Hudgens <jordan@devcamp.com>
2017-05-12 20:19:36 +0000

    add user signup form.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :cars
   resources :loads
   root to: 'welcome#home'
+  get 'signup', to: 'users#new'
+  resources :users, except:[:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
