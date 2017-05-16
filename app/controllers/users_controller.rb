de854f17db01a38bac85c764f3748ff9371a6659
Jordan Hudgens <jordan@devcamp.com>
2017-05-16 01:05:38 +0000

    add user authentication, and UI restriction.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+   before_action :set_user, only:[:edit, :update, :show]
+
   
   def index
     @users = User.all
@@ -12,23 +14,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Daves Load Manager #{@user.username}"
-      redirect_to loads_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end  
   end  
   
   def edit
-    @user = User.find(params[:id])
+   
   end
   
   def show
-     @user = User.find(params[:id])
+     
   end  
   
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       flash[:sucess] = "The account was updated successfully"
       redirect_to loads_path
@@ -38,8 +41,15 @@ class UsersController < ApplicationController
     end  
   end  
   
+  
+  
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end  
+  
+  def set_user
+    @user = User.find(params[:id])
+  end  
+  
 end  
